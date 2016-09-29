91c570b99c2dae9d0b3d3ceb483ab373bb666733
Denis Ricard <d.ricard@me.com>
2016-09-29 16:07:38 -0400

    complete day 22 challenge

--- day_22.swift
+++ day_22.swift
@@ -1,77 +1,55 @@
 // Day 22: binary search tree height
 
 // Start of Node class
-class Node<D: Comparable> {
-	var data: D
-	var left: Node?
-	var right: Node?
-	
-	init(d : D) {
-		data  = d
-	}
-} // End of Node class
+	class Node {
+		var data: Int
+		var left: Node?
+		var right: Node?
+		
+		init(d : Int) {
+			data  = d
+		}
+	} // End of Node class
 
-// Start of Tree class
-class Tree<D: Comparable> {
-	func insert(root: Node<D>?, data: D) -> Node<D>? {
-		if root == nil {
-			return Node(d: data)
-		}
-		
-		if data <= root!.data {
-			root!.left = insert(root: root!.left, data: data)
-		} else {
-			root!.right = insert(root: root!.right, data: data)
-		}
-			
-		return root
-	}
-	
-	func getHeight(root: Node<D>?) -> Int {
-		var height = -1 	// in cases where there is only the root element, the height will be 0
-		
-		// check the case where there are no nodes (root == nil)
-		// I'd return -1 in this case but the challenge doesn't specify so 0
-		guard root != nil else {
-			return 0
-		}
-		
-		func checkChildren(root: Node<D>?) -> Int {
-			
-			guard let root = root else {
-				return 0
+	// Start of Tree class
+	class Tree {
+		func insert(root: Node?, data: Int) -> Node? {
+			if root == nil {
+				return Node(d: data)
 			}
-			if root.left == nil && root.right == nil {
-				return 0
+			
+			if data <= root?.data {
+				root?.left = insert(root?.left, data: data)
 			} else {
-				if root.left != nil {
-					return 1 + checkChildren(root: root.left)
-				} else {
-					return 1 + checkChildren(root: root.right)
-				}
+				root?.right = insert(root?.right, data: data)
 			}
-		}
 				
-		height = checkChildren(root: root)
+			return root
+		}
 		
-		return height
+		func getHeight(root: Node?) -> Int {
 		
-	}	 // End of getHeight function
-} // End of Tree class
+			if let left = root!.left, let right = root!.right {
+				return 1 + max(self.getHeight(left), self.getHeight(right))
+			} else if let left = root!.left {
+				return 1 + self.getHeight(left)
+			} else if let right = root!.right {
+				return 1 + self.getHeight(right)
+			} else {
+				return 0
+			}
+					
+		} // End of getHeight function
+	} // End of Tree class
 
+var root: Node?
+var tree = Tree()
 
 var t = Int(readLine()!)!
 
-let rootData = Int(readLine()!)!
-
-var root = Node(d: rootData)
-t = t - 1
-
-var tree = Tree<Int>()
-
 while t > 0 {
-	root = tree.insert(root: root, data: Int(readLine()!)!)!
+	root = tree.insert(root, data: Int(readLine()!)!)
 	t = t - 1
 }
 
-print(tree.getHeight(root: root))
+print(tree.getHeight(root))
